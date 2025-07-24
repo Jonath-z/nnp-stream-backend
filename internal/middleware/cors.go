32925ad35787cb1af49b99c326439ee529d16d83
Jonath-z <jonathanzihindula95@gmail.com>
2025-07-24 23:35:34 +0200

    chore: add vite localhost url to the cors allowed list

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -3,7 +3,7 @@ package middleware
 import "github.com/gin-contrib/cors"
 
 var Cors = cors.New(cors.Config{
-	AllowOrigins:     []string{"http://localhost:3000", "https://nnp-stream.vercel.app", "https://nnp-stream-dashboard.vercel.app"},
+	AllowOrigins:     []string{"http://localhost:3000", "http://localhost:5173", "https://nnp-stream.vercel.app", "https://nnp-stream-dashboard.vercel.app"},
 	AllowMethods:     []string{"PUT", "PATCH", "POST", "GET", "OPTIONS"},
 	AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 	AllowCredentials: true,
