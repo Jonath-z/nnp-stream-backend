a8ac7221f6dee7152c86d4a755fe56b84d4a2f9b
Jonath-z <jonathanzihindula95@gmail.com>
2026-06-01 12:00:21 +0200

    fix: add the streaming app to the allowed cors

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -3,7 +3,14 @@ package middleware
 import "github.com/gin-contrib/cors"
 
 var Cors = cors.New(cors.Config{
-	AllowOrigins:     []string{"http://localhost:3000", "http://localhost:5173", "https://nnp-stream.vercel.app", "https://nnp-stream-dashboard.vercel.app/", "https://nnp-stream-dashboard.vercel.app"},
+	AllowOrigins: []string{
+		"http://localhost:3000",
+		"http://localhost:5173",
+		"https://nnp-stream.vercel.app",
+		"https://nnp-stream-dashboard.vercel.app/",
+		"https://nnp-stream-dashboard.vercel.app",
+		"https://stream.nnprdc.com",
+	},
 	AllowMethods:     []string{"*"},
 	AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 	AllowCredentials: true,
